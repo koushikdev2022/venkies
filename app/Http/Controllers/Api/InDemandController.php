f5336574f39332fcfaf616f40b6dc47a4dc0d091
Anuj Chauhan <anuj@solutions1313.com>
2022-10-26 13:22:58 +0530

    no

---
+++ app/Http/Controllers/Api/InDemandController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\InDemandProduct;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class InDemandController extends Controller
+{
+    public function indemand_list(){
+        $indemands=InDemandProduct::all();
+        return $this->SuccessResponse('200','list fetch successfully....',$indemands);
+    }
+    public function add_product( Request $request){
+        $validator=Validator::make($request->all(),[
+            'product_name'=>'required',
+            'source_of_information'=>'required',
+            'market_trend'=>'required',
+            'market_rate'=>'required',
+            'note'=>'required'
+            ]);
+        if($validator->fails()){
+            return $this->ErrorResponse('400',$validator->messages());
+        }
+        $result=InDemandProduct::create([
+            'user_id'=> auth()->id(),
+            'product_name'=>$request->product_name,
+            'source_of_information'=>$request->source_of_information,
+            'market_rate'=>$request->market_rate,
+            'market_trend'=>$request->market_trend,
+            'note'=>$request->note,
+        ]);
+        if(!$result){
+            return $this->ErrorResponse(400,"Something went wrong ...!");
+        }
+        return $this->SuccessResponse(200,'In-demand Product created successfully ...!', $result);
+
+    }
+}
