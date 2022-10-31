05999e93d338b20c86925f9e5dc3cd5dfa82dcdc
Anuj Chauhan <anuj@solutions1313.com>
2022-10-31 16:27:13 +0530

    no

--- app/Http/Controllers/Api/MeetingController.php
+++ app/Http/Controllers/Api/MeetingController.php
@@ -11,13 +11,11 @@ class MeetingController extends Controller
 {
     public function meeting_list(){
         $meeting= Metting::with('retailer','user')->where('user_id',auth()->id())->latest()->get();
-
         return $this->SuccessResponse(200,'Meetings fetch successfully',$meeting);
     }
     public function meeting_details($id){
         $meeting=Metting::find($id);
         $meeting->executive_name = $meeting->user->name;
-
         $meeting->retailer_name = $meeting->retailer->name ?? '';
         unset($meeting['user']);
         unset($meeting['retailer']);
