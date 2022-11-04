d2699d772dff6ff3361156feb4e7dccaea1d4f86
Anuj Chauhan <anuj@solutions1313.com>
2022-11-04 16:43:00 +0530

    no

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\Retailer;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
    public function index(){
-   return view('pages.dashboard.index');
+       $response=[
+           'total_retailers'=> Retailer::count() ,
+           'total_products' =>Product::count(),
+           'total_executives'=>User::count(),
+           'total_orders'=>Cart::count()
+       ];
+       return view('home',compact('response'));
    }
 
 }
